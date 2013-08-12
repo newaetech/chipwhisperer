98d5418e38dedb46adfa874722ba2cd081db0e8d
Colin O'Flynn <coflynn@newae.com>
2013-08-12 16:46:21 -0300

    Make module arrangement slightly less embarassing

--- software/capturev2/targets/SmartCard.py
+++ software/capturev2/targets/SmartCard.py
@@ -40,25 +40,89 @@ sys.path.append('../common')
 sys.path.append('../../openadc/controlsw/python/common')
 from ExtendedParameter import ExtendedParameter
 
-class ReaderChipWhisperer(QObject):
+class ReaderTemplate(QObject):
     paramListUpdated = Signal(list)
     
-    def __init__(self, console=None, showScriptParameter=None):
-        super(ReaderChipWhisperer, self).__init__()
-        
+    def __init__(self, console, showScriptParameter):
+        """Pass None/None if you don't have/want console/showScriptParameter"""
+        super(ReaderTemplate, self).__init__()        
         self.console = console
-        self.showScriptParameter = showScriptParameter
+        self.showScriptParameter = showScriptParameter                
+        self.setupParameters()
         
-        self.ser = None
-        ssParams = [{'name':'Baud', 'type':int, 'value':9600, 'set':self.setBaud}                                                            
+    def log(self, message):
+        self.console.append(message)
+                
+    def setupParameters(self):
+        """You should overload this. Copy/Paste into your class."""
+        ssParams = [{'name':'Example Parameter', 'type':'int', 'value':5, 'set':self.setSomething}]        
+        self.params = Parameter.create(name='Smartcard Reader', type='group', children=ssParams)
+        ExtendedParameter.setupExtended(self.params, self)      
+        
+    def setSomething(self):
+        """Here you would send value to the reader hardware"""
+        pass 
+
+    def sendAPDU(self):
+        """Send APDU to SmartCard, get Response"""
+        pass
+    
+    def con(self, oa=None):
+        """Connect to reader. oa parameter is OpenADC/ChipWhisperer hardware, only used to integrated readers"""
+        pass
+    
+    def flush(self):
+        """Discard all input buffers"""
+        pass
+    
+    def reset(self):
+        """Reset card & save the ATR"""
+        pass
+    
+    def getATR(self):
+        """Get the ATR from the SmartCard. Reads a saved value, user reset() to actually reset card."""
+        pass
+
+class ReaderChipWhisperer(ReaderTemplate):
+    
+    def __init__(self, console=None, showScriptParameter=None):
+        super(ReaderChipWhisperer, self).__init__(console, showScriptParameter)
+        
+
+    def setupParameters(self):
+        ssParams = [{'name':'Baud', 'type':'int', 'value':9600, 'set':self.setBaud}                                                            
                     ]        
         self.params = Parameter.create(name='Target Connection', type='group', children=ssParams)
         ExtendedParameter.setupExtended(self.params, self)       
 
-    def sendAPDU(self):
+    def setBaud(self, brate):
+        """Set baud rate of reader"""
+        
+        #For SmartCard we assume Stop-bits = 1, guard-bits=1, start-bits=1, parity=even
+        
         pass
     
-    def getResponse(self):
+    def sendAPDU(self):
+        """Send APDU to SmartCard, get Response"""
+        pass
+    
+    def con(self, oa=None):
+        """Connect to reader. oa parameter is OpenADC/ChipWhisperer hardware, only used to integrated readers"""
+        
+        
+        self.setBaud(9600)
+        
+    
+    def flush(self):
+        """Discard all input buffers"""
+        pass
+    
+    def reset(self):
+        """Reset card & save the ATR"""
+        pass
+    
+    def getATR(self):
+        """Get the ATR from the SmartCard. Reads a saved value, user reset() to actually reset card."""
         pass
     
 class ProtocolSASEBOWCardOS(QObject):
@@ -92,7 +156,7 @@ class SmartCard(QObject):
         self.showScriptParameter = showScriptParameter
         
         self.ser = None
-        ssParams = [{'name':'Reader Hardware', 'type':'list', 'values':{"ChipWhisperer-Connected":None, "PC/SC Reader":None, "Cheapskate-Serial":None}, 'value':None, 'set':self.setConnection},
+        ssParams = [{'name':'Reader Hardware', 'type':'list', 'values':{"ChipWhisperer-Connected":ReaderChipWhisperer(), "PC/SC Reader":None, "Cheapskate-Serial":None}, 'value':None, 'set':self.setConnection},
                     #"BasicCard v7.5 (INCOMPLETE"):None, 
                     #"Custom (INCOMPLETE)":None, "DPAContestv4 (INCOMPLETE)":None
                     {'name':'SmartCard Protocol', 'type':'list', 'values':{"SASEBO-W SmartCard OS":None, "ChipWhisperer-Dumb":None}, 'value':None, 'set':self.setProtocol}                    
