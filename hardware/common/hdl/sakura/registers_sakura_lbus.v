11996924fb9298045190323bf9ea4d76a4e33736
Colin O'Flynn <coflynn@newae.com>
2013-09-28 08:19:54 -0300

    Disable CHIPSCOPE in commited file

--- hardware/common/hdl/sakura/registers_sakura_lbus.v
+++ hardware/common/hdl/sakura/registers_sakura_lbus.v
@@ -1,5 +1,5 @@
 `include "includes.v"
-`define CHIPSCOPE
+//`define CHIPSCOPE
 
 /***********************************************************************
 This file is part of the OpenADC Project. See www.newae.com for more details,
