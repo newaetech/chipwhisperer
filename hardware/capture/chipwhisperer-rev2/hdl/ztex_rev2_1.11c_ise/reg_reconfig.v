eea5f08a888e55649e71c034398913de84668853
Colin O'Flynn <coflynn@newae.com>
2013-11-29 13:57:30 -0400

    Add copyright notice + docs to reg_reconfig.v

--- hardware/capture/chipwhisperer-rev2/hdl/ztex_rev2_1.11c_ise/reg_reconfig.v
+++ hardware/capture/chipwhisperer-rev2/hdl/ztex_rev2_1.11c_ise/reg_reconfig.v
@@ -1,24 +1,37 @@
 `timescale 1ns / 1ps
 //`define CHIPSCOPE
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    20:29:19 11/07/2013 
-// Design Name: 
-// Module Name:    reg_reconfig 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
+/***********************************************************************
+This file is part of the OpenADC Project. See www.newae.com for more details,
+or the codebase at http://www.assembla.com/spaces/openadc .
+
+This file is the Partial Reconfiguration (using ICAP) interface.
+
+Copyright (c) 2013, Colin O'Flynn <coflynn@newae.com>. All rights reserved.
+This project (and file) is released under the 2-Clause BSD License:
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+   * Redistributions of source code must retain the above copyright notice,
+	  this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above copyright
+	  notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*************************************************************************/
+
 module reg_reconfig(
 	input 			reset_i,
 	input 			clk,
@@ -74,8 +87,19 @@ module reg_reconfig(
 	 [ 7 6 5 4 3 2 1 0 ]
 	   x x R G G G G G               
 		
-		G G G G G = 1 1 0 1 0 (0x1A)
-		
+		      G G G G G = 1 1 0 1 0 (0x1A) start pattern
+			 R = Reset bit
+			 
+	 The first byte is written to the status register, remaining bytes written to FIFO.
+	 
+	 To use interface:
+	 * Split bitstream into byte chunks
+	 * Write (1<<5) to reconfig addr
+	 * Write 0x00 to reconfig addr
+	 * Prepend a 0 to bitstream chunks (required as first byte will go into status/control register)
+	 * Write chunks with prepended 0 into reconfig addr
+	 * Write 0x1A to reconfig addr (start pattern)
+	 * Write 0x00 to reconfig addr		
 	*/
 	reg [15:0] reg_hyplen_reg;
 	assign reg_hyplen = reg_hyplen_reg;
