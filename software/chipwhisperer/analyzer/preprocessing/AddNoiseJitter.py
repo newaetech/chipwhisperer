645b6e436eef11b5cc27532bf489b9b58d94ab0d
Colin O'Flynn <coflynn@newae.com>
2014-05-14 20:27:14 -0300

    SW: CW: Preprocessing: Fix documentation in addnoisejitter

--- software/chipwhisperer/analyzer/preprocessing/AddNoiseJitter.py
+++ software/chipwhisperer/analyzer/preprocessing/AddNoiseJitter.py
@@ -79,72 +79,20 @@ class AddNoiseJitter(PreprocessingBase):
 # This function stolen from: http://stackoverflow.com/questions/2777907/python-numpy-roll-with-padding
 def roll_zeropad(a, shift, axis=None):
     """
-    Roll array elements along a given axis.
+    Roll array elements along a given axis. Elements rolled off the end of the array are treated as zeros.
 
-    Elements off the end of the array are treated as zeros.
+    :param a: Input Array
+    :type a: array_like
+    
+    :param shift: The number of places by which elements are shifted.
+    :type shift: int
 
-    Parameters
-    ----------
-    a : array_like
-        Input array.
-    shift : int
-        The number of places by which elements are shifted.
-    axis : int, optional
-        The axis along which elements are shifted.  By default, the array
-        is flattened before shifting, after which the original
-        shape is restored.
+    :param axis: The axis along which elements are shifted.  By default, the array
+                 is flattened before shifting, after which the original
+                 shape is restored.
+    :type axis: int, optional      
 
-    Returns
-    -------
-    res : ndarray
-        Output array, with the same shape as `a`.
-
-    See Also
-    --------
-    roll     : Elements that roll off one end come back on the other.
-    rollaxis : Roll the specified axis backwards, until it lies in a
-               given position.
-
-    Examples
-    --------
-    >>> x = np.arange(10)
-    >>> roll_zeropad(x, 2)
-    array([0, 0, 0, 1, 2, 3, 4, 5, 6, 7])
-    >>> roll_zeropad(x, -2)
-    array([2, 3, 4, 5, 6, 7, 8, 9, 0, 0])
-
-    >>> x2 = np.reshape(x, (2,5))
-    >>> x2
-    array([[0, 1, 2, 3, 4],
-           [5, 6, 7, 8, 9]])
-    >>> roll_zeropad(x2, 1)
-    array([[0, 0, 1, 2, 3],
-           [4, 5, 6, 7, 8]])
-    >>> roll_zeropad(x2, -2)
-    array([[2, 3, 4, 5, 6],
-           [7, 8, 9, 0, 0]])
-    >>> roll_zeropad(x2, 1, axis=0)
-    array([[0, 0, 0, 0, 0],
-           [0, 1, 2, 3, 4]])
-    >>> roll_zeropad(x2, -1, axis=0)
-    array([[5, 6, 7, 8, 9],
-           [0, 0, 0, 0, 0]])
-    >>> roll_zeropad(x2, 1, axis=1)
-    array([[0, 0, 1, 2, 3],
-           [0, 5, 6, 7, 8]])
-    >>> roll_zeropad(x2, -2, axis=1)
-    array([[2, 3, 4, 0, 0],
-           [7, 8, 9, 0, 0]])
-
-    >>> roll_zeropad(x2, 50)
-    array([[0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0]])
-    >>> roll_zeropad(x2, -50)
-    array([[0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0]])
-    >>> roll_zeropad(x2, 0)
-    array([[0, 1, 2, 3, 4],
-           [5, 6, 7, 8, 9]])
+    :rtype: ndarray, with the same shape as ``a``
 
     """
     a = np.asanyarray(a)
