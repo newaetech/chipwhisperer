59a2bd2858ec6667c5aebdbca018a2a2e88d3e48
Colin O'Flynn <coflynn@newae.com>
2014-10-17 18:18:07 -0300

    SW: Fix MySQL Driver, hadn't been kept updated with latest changes

--- software/chipwhisperer/common/traces/TraceContainerMySQL.py
+++ software/chipwhisperer/common/traces/TraceContainerMySQL.py
@@ -40,7 +40,7 @@ except ImportError, e:
 from openadc.ExtendedParameter import ExtendedParameter
 from TraceContainerConfig import makeAttrDict
 
-class parameters(object):    
+class parameters(object):
     def __init__(self, openMode=False):
         self.fmt = None
         traceParams = [{'name':'MySQL Configuration', 'type':'group', 'children':[
@@ -51,86 +51,86 @@ class parameters(object):
                         {'name':'Database', 'key':'database', 'type':'str', 'value':'CWTraces'},
                         {'name':'Table Name', 'key':'tableName', 'type':'str', 'value':'', 'readonly':True}
                       ]}]
-        
+
         if openMode == False:
-            traceParams[0]['children'].append({'name':'Table Naming', 'key':'tableNameType', 'type':'list', 'values':{'Auto-Prefix':'prefix'}, 'value':'prefix'})   
-        else:            
+            traceParams[0]['children'].append({'name':'Table Naming', 'key':'tableNameType', 'type':'list', 'values':{'Auto-Prefix':'prefix'}, 'value':'prefix'})
+        else:
             traceParams[0]['children'].append({'name':'Relist Tables', 'key':'tableListAct', 'type':'action'})
-            traceParams[0]['children'].append({'name':'Table List', 'key':'tableNameList', 'type':'list', 'values':[], 'value':'', 'linked':['Table Name']})  
-           
+            traceParams[0]['children'].append({'name':'Table List', 'key':'tableNameList', 'type':'list', 'values':[], 'value':'', 'linked':['Table Name']})
+
         traceParams[0]['children'].append({'name':'Trace Format', 'key':'traceFormat', 'type':'list', 'values':['NumPy Pickle'], 'value':'NumPy Pickle', 'set':self.setFormat})
-        
+
         self.traceParams = traceParams
-        
+
         self.params = Parameter.create(name='MySQL Settings', type='group', children=traceParams)
         ExtendedParameter.setupExtended(self.params, self)
-        
+
     def setFormat(self, fmt):
         self.fmt = fmt
-        
+
     def format(self):
         if self.fmt is None:
             self.fmt = self.findParam('traceFormat').value()
-            
+
         return self.fmt
-        
+
     def paramList(self):
         return [self.params]
 
-class TraceContainerMySQL(TraceContainer.TraceContainer):   
+class TraceContainerMySQL(TraceContainer.TraceContainer):
     getParamsClass = parameters
     getParams = parameters()
-    
+
     def __init__(self, params=None):
         super(TraceContainerMySQL, self).__init__()
         self.db = None
         self.idOffset = 0
         self.lastId = 0
-        
+
         if params is not None:
             self.getParams = params
-                        
+
         #Connect actions if applicable
-        try:            
+        try:
             self.getParams.findParam('tableListAct').opts['action'] = self.listAllTables
         except AttributeError:
             pass
-             
+
         self.getParams.findParam('tableName').opts['get'] = self._getTableName
-            
+
         #Save extra configuration options
         self.attrDict = makeAttrDict("MySQL Config", "mysql", self.getParams.traceParams)
         self.config.attrList.append(self.attrDict)
-            
+
         #Format name must agree with names from TraceContainerFormatList
         self.config.setAttr("format", "mysql")
-            
+
     def makePrefix(self, mode='prefix'):
         if mode == 'prefix':
             prefix = self.config.attr('prefix')
             if prefix == "" or prefix is None:
                 raise AttributeError("Prefix attribute not set in trace config")
-            
+
             #Drop everything but underscore (_) for table name
             prefix = "tracedb_" + prefix
             prefix = prefix.replace("-","_")
             prefix = ''.join(c for c in prefix if c.isalnum() or c in("_"))
-            
+
             return prefix
-        
+
         raise ValueError("Invalid mode: %s"%mode)
-            
+
     def prepareDisk(self):
-        self.con()        
+        self.con()
         #CREATE DATABASE `cwtraces` /*!40100 COLLATE 'utf8_unicode_ci' */
         traceprefix = self.makePrefix(self.getParams.findParam('tableNameType').value())
-        
+
         #Check version as simple validation
         result = self.db.query("SELECT VERSION()")
         print "MySQL Version: %s"%result.rows[0][0]
-         
+
         self.tableName = traceprefix
-         
+
         self.db.query("CREATE TABLE IF NOT EXISTS %s(Id INT PRIMARY KEY AUTO_INCREMENT,\
          Textin VARCHAR(32),\
          EncKey VARCHAR(32),\
@@ -140,54 +140,63 @@ class TraceContainerMySQL(TraceContainer.TraceContainer):
     def con(self):
         if self.db is not None:
             self.db.close()
-            
+
         db = sql.Connection()
-        
+
         server = self.getParams.findParam('addr').value()
         port = int(self.getParams.findParam('port').value())
         user = self.getParams.findParam('user').value()
         password = self.getParams.findParam('password').value()
         database = self.getParams.findParam('database').value()
-        
+
         #Connection
         db.connect(server,port,user,password,database)
-        
+
         self.db = db
-        
+
     def _getTableName(self):
         return self.getParams.findParam('tableNameList').value()
-        
+
     def listAllTables(self):
         self.con()
         database = self.getParams.findParam('database').value()
         results = self.db.query("SHOW TABLES IN %s"%database)
         tables = []
         for r in results.rows:
-            tables.append(r[0])            
+            tables.append(r[0])
         self.getParams.findParam('tableNameList').setLimits(tables)
-        
-        
+
+
+    def updatePointsTraces(self):
+        res = self.db.query("SELECT COUNT(*) FROM %s" % self.tableName)
+        self._numTraces = res.rows[0][0]
+
+        wav = self.db.query("SELECT Wave FROM %s LIMIT 1 OFFSET %d" % (self.tableName, 0)).rows[0][0]
+        self._numPoints = self.formatWave(wav, read=True).shape[0]
+
     def updateConfigData(self):
         self.con()
-        self.tableName = self.getParams.findParam('tableName').value()        
-        res = self.db.query("SELECT COUNT(*) FROM %s"%(self.tableName))
-        self._NumTrace = res.rows[0][0]
+
+        self.tableName = self.getParams.findParam('tableName').value()
+        res = self.db.query("SELECT COUNT(*) FROM %s" % self.tableName)
+        self._numTraces = res.rows[0][0]
         self.config.setAttr('numTraces', self._NumTrace)
-        
+
         wav = self.db.query("SELECT Wave FROM %s LIMIT 1 OFFSET %d"%(self.tableName, 0)).rows[0][0]
-        self._NumPoint = self.formatWave(wav, read=True).shape[0]
+        self._numPoints = self.formatWave(wav, read=True).shape[0]
         self.config.setAttr('numPoints', self._NumPoint)
-        
+
     def numTraces(self, update=False):
         if update:
             self.updateConfigData()
-        return self._NumTrace
-        
+        return self._numTraces
+
+
     def numPoints(self, update=False):
         if update:
             self.updateConfigData()
-        return self._NumPoint
-        
+        return self._numPoints
+
     def loadAllConfig(self):
         for p in self.getParams.traceParams[0]['children']:
             try:
@@ -196,10 +205,10 @@ class TraceContainerMySQL(TraceContainer.TraceContainer):
             except ValueError:
                 pass
             #print "%s to %s=%s"%(p["key"], val, self.getParams.findParam(p["key"]).value())
-        
+
     def loadAllTraces(self, path=None, prefix=None):
         self.updateConfigData()
-        
+
     def formatWave(self, wave, read=False):
         if self.getParams.format() == "NumPy Pickle":
             if read == False:
@@ -208,41 +217,54 @@ class TraceContainerMySQL(TraceContainer.TraceContainer):
                 return np.array(pickle.loads(wave))
         else:
             raise AttributeError("Invalid Format for MySQL")
-        
-    def addTrace(self, trace, textin, textout, key, dtype=np.double):      
+
+    def addTrace(self, trace, textin, textout, key, dtype=np.double):
         strTextin = ""
         for t in textin:
             strTextin += "%02X"%t
 
         strTextout = ""
         for t in textout:
-            strTextout += "%02X"%t            
-        
+            strTextout += "%02X" % t
+
         if key is not None:
             strKey = ""
             for t in key:
                 strKey += "%02X"%t
         else:
-            strKey = ""            
-            
+            strKey = ""
+
         self.db.query("INSERT INTO %s(Textin, Textout, EncKey, Wave) VALUES('%s', '%s', '%s', "%(self.tableName, strTextin, strTextout,
                                                                                                 strKey) + "%s)", (self.formatWave(trace),))
-        
-    def saveAll(self):        
+
+    def saveAll(self):
         #Save attributes from config settings
         for t in self.getParams.traceParams[0]['children']:
             self.config.setAttr(t["key"],  self.getParams.findParam(t["key"]).value() ,"mysql")
-            
+
         #Save table name/prefix too
         self.config.setAttr("tableName", self.tableName, "mysql")
         self.config.saveTrace()
-        
-    def closeAll(self):
-        self.saveAll()
-        
+
+    def closeAll(self, clearTrace=True, clearText=True, clearKeys=True):
+        # self.saveAllTraces(os.path.dirname(self.config.configFilename()), prefix=self.config.attr("prefix"))
+
+        # Release memory associated with data in case this isn't deleted
+        if clearTrace:
+            self.traces = None
+
+        if clearText:
+            self.textins = None
+            self.textouts = None
+
+        if clearKeys:
+            self.keylist = None
+            self.knownkey = None
+
         if self.db is not None:
             self.db.close()
         self.db = None
+
 
     def getTrace(self, n):
         wv = self.db.query("SELECT Wave FROM %s LIMIT 1 OFFSET %d"%(self.tableName, n)).rows[0][0]
@@ -252,7 +274,7 @@ class TraceContainerMySQL(TraceContainer.TraceContainer):
         lst = []
         for i in range(0,len(asc),2):
             lst.append( int(asc[i:(i+2)], 16) )
-        return lst            
+        return lst
 
     def getTextin(self, n):
         asc = self.db.query("SELECT Textin FROM %s LIMIT 1 OFFSET %d"%(self.tableName, n)).rows[0][0]
@@ -264,6 +286,9 @@ class TraceContainerMySQL(TraceContainer.TraceContainer):
 
     def getKnownKey(self, n=None):
         if n is None:
-            n = 0    
+            n = 0
         asc = self.db.query("SELECT EncKey FROM %s LIMIT 1 OFFSET %d"%(self.tableName, n)).rows[0][0]
         return self.asc2list(asc)
+
+
+
