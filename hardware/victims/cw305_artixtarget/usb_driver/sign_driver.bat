679783017dfc4a9c6fe5f39bc55435f1bc3e15cf
Colin O'Flynn <coflynn@newae.com>
2016-02-26 15:39:27 -0400

    HW: CW305: Remove unneeded script for drivers

--- hardware/victims/cw305_artixtarget/usb_driver/sign_driver.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files\Windows Kits\8.1\bin\x86\inf2cat" /v /driver:%~dp0 /os:XP_X86,Vista_X86,Vista_X64,7_X86,7_X64,8_X86,8_X64,6_3_X86,6_3_X64
-"C:\Program Files\Windows Kits\8.1\bin\x86\signtool" sign /v /n "NewAE Technology Inc." /tr http://timestamp.globalsign.com/scripts/timestamp.dll *.cat
-pause
