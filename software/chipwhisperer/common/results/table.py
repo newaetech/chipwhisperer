397539aa6ee0a109f8917555bafea93f029850fb
Colin O'Flynn <coflynn@newae.com>
2016-04-22 12:02:54 -0300

    SW: Analyzer resizes PGE table correctly again

--- software/chipwhisperer/common/results/table.py
+++ software/chipwhisperer/common/results/table.py
@@ -49,7 +49,7 @@ class ResultsTable(QTableWidget, ResultsWidgetBase, ActiveAnalysisObserver):
         self.useSingle = False
         self.updateMode = self.findParam('updateMode').value()
 
-        stdCell = QTableWidgetItem("")
+        stdCell = QTableWidgetItem(" \n ")
         stdCell.setFlags(stdCell.flags() ^ Qt.ItemIsEditable)
         stdCell.setTextAlignment(Qt.AlignCenter)
 
@@ -61,6 +61,8 @@ class ResultsTable(QTableWidget, ResultsWidgetBase, ActiveAnalysisObserver):
             self.setItem(0, x, cell)
             for y in range(1, self.numPerms+1):
                 self.setItem(y, x, stdCell.clone())
+
+        self.resizeRowsToContents()
 
         self.setVerticalHeaderItem(0, QTableWidgetItem("PGE"))
         for y in range(1, self.numPerms+1):
