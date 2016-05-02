f6db939fe23afbf939325cd2607a542e564c6f50
Colin O'Flynn <coflynn@newae.com>
2016-05-02 15:13:28 -0300

    SW: Fix pico/visa scopes

--- software/chipwhisperer/capture/scopes/VisaScope.py
+++ software/chipwhisperer/capture/scopes/VisaScope.py
@@ -33,7 +33,7 @@ from chipwhisperer.common.utils import pluginmanager
 class VisaScopeInterface(ScopeTemplate):
     name =  "VISA Scope"
 
-    def __init__(self, parentParam):
+    def __init__(self, parentParam=None):
         super(VisaScopeInterface, self).__init__(parentParam)
         self.setupActiveParams([lambda: self.lazy(self), lambda: self.lazy(self.scopetype)])
         scopes = pluginmanager.getPluginsInDictFromPackage("chipwhisperer.capture.scopes.visascope_interface", True, False, self)
