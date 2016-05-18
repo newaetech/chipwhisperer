2bdac8aa7bee35a9c8289091c8eb218271ee8b08
Greg d'Eon <gdeon@newae.com>
2016-05-18 09:56:36 -0300

    Added script for tutorial B3-1

---
+++ software/chipwhisperer/capture/scripts/cwlite-timingattack.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2013-2016, NewAE Technology Inc
+# All rights reserved.
+#
+# Authors: Colin O'Flynn, Greg d'Eon
+#
+# Find this and more at newae.com - this file is part of the chipwhisperer
+# project, http://www.assembla.com/spaces/chipwhisperer
+#
+#    This file is part of chipwhisperer.
+#
+#    chipwhisperer is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    chipwhisperer is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with chipwhisperer.  If not, see <http://www.gnu.org/licenses/>.
+#=================================================
+#
+
+"""
+This script is an example of a timing attack on a simple password checker. 
+It is the result of Tutorial B3-1 from the ChipWhisperer Wiki.
+
+Usage: ::
+    python cwlite-timingattack.py
+"""
+
+import sys
+from chipwhisperer.common.api.CWCoreAPI import CWCoreAPI  # Import the ChipWhisperer API
+import chipwhisperer.capture.ui.CWCaptureGUI as cwc       # Import the ChipWhispererCapture GUI
+from chipwhisperer.common.scripts.base import UserScriptBase
+
+
+class UserScript(UserScriptBase):
+    _name = "Timing Attack on CW-Lite (XMEGA)"
+    _description = "SimpleSerial Timing Attack on CW-Lite (XMEGA)"
+
+    def __init__(self, api):
+        super(UserScript, self).__init__(api)
+
+    def run(self):
+        """
+        Set up the CWLite to record power traces while attempting passwords
+        and perform a timing attack to recover the password.
+        
+        Called when the script is run (from command line or from CW-Capture)
+        """
+        
+        # First: set up the basics and connect to the CW-Lite
+        self.api.setParameter(['Generic Settings', 'Scope Module', 'ChipWhisperer/OpenADC'])
+        self.api.setParameter(['Generic Settings', 'Target Module', 'Simple Serial'])
+        self.api.setParameter(['Generic Settings', 'Trace Format', 'ChipWhisperer/Native'])
+        self.api.setParameter(['Simple Serial', 'Connection', 'ChipWhisperer-Lite'])
+        self.api.setParameter(['ChipWhisperer/OpenADC', 'Connection', 'ChipWhisperer-Lite'])
+        self.api.connect()
+        
+        
+        # Next: set up everything we need to connect to the target
+        # Put all of our commands in a list and execute them at the end
+        lstexample = [
+                      # Gain
+                      ['OpenADC', 'Gain Setting', 'Setting', 45],
+                      # Trigger
+                      ['OpenADC', 'Trigger Setup', 'Mode', 'rising edge'],
+                      ['OpenADC', 'Trigger Setup', 'Offset', 0],
+                      ['OpenADC', 'Trigger Setup', 'Total Samples', 2000],
+                      # Clock
+                      ['OpenADC', 'Clock Setup', 'CLKGEN Settings', 'Desired Frequency', 7370000.0],
+                      ['OpenADC', 'Clock Setup', 'ADC Clock', 'Source', 'CLKGEN x4 via DCM'],
+                      ['OpenADC', 'Clock Setup', 'ADC Clock', 'Reset ADC DCM', None],
+                      # Pins
+                      ['CW Extra Settings', 'Trigger Pins', 'Target IO4 (Trigger Line)', True],
+                      ['CW Extra Settings', 'Target HS IO-Out', 'CLKGEN'],
+                      ['CW Extra Settings', 'Target IOn Pins', 'Target IO1', 'Serial RXD'],
+                      ['CW Extra Settings', 'Target IOn Pins', 'Target IO2', 'Serial TXD'],
+                      # Automatic commands
+                      ['Simple Serial', 'Load Key Command', ''],
+                      ['Simple Serial', 'Go Command', 'h0px3\n'],
+                      ['Simple Serial', 'Output Format', ''],
+                      # Auto-reset
+                      ['Generic Settings', 'Auxiliary Module', 'Reset AVR/XMEGA via CW-Lite'],
+                      ['Reset AVR/XMEGA via CW-Lite', 'Delay (Post-Arm)', 1200],
+                      ]
+        
+        #Download all hardware setup parameters
+        for cmd in lstexample: 
+            self.api.setParameter(cmd)
+                       
+        # Get one capture for fun
+        self.api.capture1()
+        data = self.api.getScope().datapoints
+        print data
+        
+        
+        # Crack the first letter
+        password = ''
+        trylist = 'abcdefghijklmnopqrstuvwxyz0123456789'
+          
+        for i in range(5):
+            for c in trylist:
+                # Get a power trace using our next attempt
+                nextPass = password + '{}'.format(c)
+                self.api.setParameter(['Simple Serial', 'Go Command', '{}\n'.format(nextPass)])
+                self.api.capture1()
+                
+                # Grab the trace
+                nextTrace = self.api.getScope().datapoints
+                
+                # Check location 153, 225, etc. If it's too low, we've failed
+                if nextTrace[153 + 72*i] < -0.2:
+                    continue
+                    
+                # If we got here, we've found the right letter
+                password += c
+                print '{} characters: {}'.format(i+1, password)
+                break
+
+
+if __name__ == '__main__':
+    # Startup code: this is only executed if we run our script from the command line
+    # Probably doesn't need to be changed
+    app = cwc.makeApplication()     # Make the GUI application (even if we don't use it)
+    api = CWCoreAPI()               # Make the API 
+    gui = cwc.CWCaptureGUI(api)     # Make a GUI (even if we don't use it)
+    gui.show()                      # Optional: show the GUI
+    
+    # Run our script
+    usercommands = UserScript(api)                  # Pass the API as parameter to the User Script
+    usercommands.run()                              # Run the User Script
+
+    # Let the program continue running
+    sys.exit(app.exec_())
