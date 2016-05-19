dce610481f3af604021d7d640d925c926a82655e
Colin O'Flynn <coflynn@newae.com>
2016-05-19 10:20:49 -0300

    SW: Small fix for TE...

--- software/chipwhisperer/analyzer/utils/TraceExplorerScripts/PartitionDisplay.py
+++ software/chipwhisperer/analyzer/utils/TraceExplorerScripts/PartitionDisplay.py
@@ -188,7 +188,7 @@ class POI(QWidget):
 
     def savePOI(self):
         poiDict = {"poi":self.poiArray}
-        self.parent.parent.parent.project().addDataConfig(poiDict, "Template Data", "Points of Interest")
+        CWCoreAPI.getInstance().project().addDataConfig(poiDict, "Template Data", "Points of Interest")
 
     def calcPOI(self, numMax, pointRange, minSpace, diffs=None):
         if diffs:
