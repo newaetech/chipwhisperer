9909c878ed5e6a963c21e07eccbb54377cd4e124
phonchi <steve2003121@gmail.com>
2016-06-08 04:33:46 +0800

    sphinx update

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -11,8 +11,17 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
-import sphinx_rtd_theme
+import os 
+import sys
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    sphinx_rtd_theme = None
+
+try:
+    from sphinxcontrib import spelling
+except ImportError:
+    spelling = None
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -38,10 +47,21 @@ sys.path.insert(0, os.path.abspath('../../software/chipwhisperer/api/utils'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinx.ext.viewcode']
+extensions = [
+	'sphinx.ext.autodoc',
+	'sphinx.ext.doctest', #Addes
+	'sphinx.ext.intersphinx', #Added
+	'sphinx.ext.todo', 
+	'sphinx.ext.pngmath', 
+	'sphinx.ext.viewcode'
+]
 
+if spelling is not None:
+    extensions.append('sphinxcontrib.spelling')
+	
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+nitpicky = True
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -54,7 +74,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ChipWhisperer'
-copyright = u'2014, NewAE Technology Inc, Colin O\'Flynn'
+copyright = u'2014-2016, NewAE Technology Inc, Colin O\'Flynn'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -104,7 +124,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+if sphinx_rtd_theme:
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+    html_theme = "default"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -112,7 +136,6 @@ html_theme = "sphinx_rtd_theme"
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -123,12 +146,10 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'sidebarlogo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -202,7 +223,6 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = 'modtheme/static/sidebarlogo.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
@@ -253,3 +273,12 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {'https://docs.python.org/3': None}
+
+epub_theme = 'epub'
+
+# Retry requests in the linkcheck builder so that we're resillient against
+# transient network errors.
+linkcheck_retries = 2
