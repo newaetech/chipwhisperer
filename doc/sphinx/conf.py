7bdc28721cf0e74517f9258bbb3a7ccd337f0cbb
phonchi <steve2003121@gmail.com>
2016-06-08 11:05:00 +0800

    test17

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -28,7 +28,7 @@ except ImportError:
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../software/'))
 sys.path.insert(0, os.path.abspath('../../software/chipwhisperer/'))
 sys.path.insert(0, os.path.abspath('../../software/chipwhisperer/analyzer'))
