e413f21f789f9e9213b6d9ee4a49ef368fbc9ea7
Colin O'Flynn <coflynn@newae.com>
2016-06-09 00:16:51 -0300

    SW: Add CC programming support (highly experimental)

--- hardware/capture/chipwhisperer-lite/sam3u_fw/SAM3U_VendorExample/src/usb.c
+++ hardware/capture/chipwhisperer-lite/sam3u_fw/SAM3U_VendorExample/src/usb.c
@@ -25,6 +25,8 @@
 #include "pdi\XPROGTimeout.h"
 #include "pdi\XPROGTarget.h"
 #include "isp\V2Protocol.h"
+#include "ccdebug\chipcon.h"
+
 #include "usart_driver.h"
 #include "usb.h"
 #include "scard_usb.h"
@@ -102,6 +104,7 @@ void main_vendor_disable(void)
 #define REQ_XMEGA_PROGRAM 0x20
 #define REQ_AVR_PROGRAM 0x21
 #define REQ_SAM3U_CFG 0x22
+#define REQ_CC_PROGRAM 0x23
 
 
 COMPILER_WORD_ALIGNED static uint8_t ctrlbuffer[64];
@@ -125,6 +128,7 @@ void ctrl_progfpga_bulk(void);
 bool ctrl_xmega_program(void);
 void ctrl_xmega_program_void(void);
 void ctrl_avr_program_void(void);
+void ctrl_cc_program_void(void);
 
 void ctrl_readmem_bulk(void){
 	uint32_t buflen = *(CTRLBUFFER_WORDPTR);	
@@ -206,6 +210,10 @@ void ctrl_avr_program_void(void)
 	V2Protocol_ProcessCommand();
 }
 
+void ctrl_cc_program_void(void)
+{
+	CCProtocol_ProcessCommand();
+}
 
 static void ctrl_sam3ucfg_cb(void)
 {
@@ -346,6 +354,11 @@ bool main_setup_out_received(void)
 			udd_g_ctrlreq.callback = ctrl_sam3ucfg_cb;
 			return true;
 			
+		/* ChipCon (TI) Programming */
+		case REQ_CC_PROGRAM:
+			udd_g_ctrlreq.callback = ctrl_cc_program_void;
+			return true;
+			
 		default:
 			return false;
 	}					
@@ -424,6 +437,10 @@ bool main_setup_in_received(void)
 			
 		case REQ_AVR_PROGRAM:
 			return V2Protocol_ProcessCommand();
+			break;
+			
+		case REQ_CC_PROGRAM:
+			return CCProtocol_ProcessCommand();
 			break;
 			
 		case REQ_USART0_CONFIG:
