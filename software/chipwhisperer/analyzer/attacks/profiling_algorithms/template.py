696903f234e91efd70ac14f80e28214963d371d2
Adriel Ziesemer Jr <aziesemer@gmail.com>
2016-06-10 21:32:33 -0300

    Test script for the template attack now performs findPOI() as well. But is not working :(

--- software/chipwhisperer/analyzer/attacks/profiling_algorithms/template.py
+++ software/chipwhisperer/analyzer/attacks/profiling_algorithms/template.py
@@ -194,6 +194,7 @@ class ProfilingTemplate(AutoScript, PassiveTraceObserver, Plugin):
         # Hack for now - just use last template found
         template = self.loadTemplatesFromProject()[-1]
         pois = template["poi"]
+        print str(pois)
         numparts = len(template['mean'][0])
         results = np.zeros((16, 256))
 
