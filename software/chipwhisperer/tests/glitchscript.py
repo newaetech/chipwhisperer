4be56c91169362a6a61a705633ac6b3e3fc5f80c
Adriel Ziesemer Jr <aziesemer@gmail.com>
2016-06-16 15:03:36 -0300

    Improved glitch attack documentation

--- software/chipwhisperer/tests/glitchscript.py
+++ software/chipwhisperer/tests/glitchscript.py
@@ -68,13 +68,14 @@ class UserScript(UserScriptBase):
         ]
         for cmd in lstexample: self.api.setParameter(cmd)
 
-        # Software Setup - 2. Setup the CLKGEN Module to Generate a 7.37 MHz clock and route it through the Glitch Generator
+        print "Software Setup - 3. Connect the Serial Port"
         lstexample = [
             ['CW Extra Settings', 'Target IOn Pins', 'Target IO1', 'Serial RXD'],
             ['CW Extra Settings', 'Target IOn Pins', 'Target IO2', 'Serial TXD']
         ]
         for cmd in lstexample: self.api.setParameter(cmd)
 
+        print "Software Setup - 3.4. Flashing test firmware"
         xmega = XMEGAProgrammer()
         xmega.setUSBInterface(self.api.getScope().scopetype.dev.xmega)
         xmega._logging = None
@@ -83,62 +84,65 @@ class UserScript(UserScriptBase):
         xmega.program(r"glitchsimple.hex", memtype="flash", verify=True)
         xmega.close()
 
+        print "Manual Glitch Trigger"
         lstexample = [
             ['Glitch Module', 'Glitch Width (as % of period)', 10],
             ['Glitch Module', 'Glitch Offset (as % of period)', 26.6],
             ['Glitch Module', 'Repeat', 105],
             ['Glitch Module', 'Glitch Trigger', 'Manual'],
-            ['Glitch Module', 'Manual Trigger / Single-Shot Arm', None],
+            ['Glitch Module', 'Manual Trigger / Single-Shot Arm', None],  # Push the button
         ]
         for cmd in lstexample: self.api.setParameter(cmd)
 
+        print "Automatically Resetting Target"
         lstexample = [
             ['Generic Settings', 'Auxiliary Module', 'Reset AVR/XMEGA via CW-Lite'],
+            ['Aux Settings', 'Reset AVR/XMEGA via CW-Lite', 'Interface', 'xmega (PDI)'],
+            ['Aux Settings', 'Reset AVR/XMEGA via CW-Lite', 'Test Reset', None],  # Push the button
             ['Simple Serial', 'Load Key Command', u''],
             ['Simple Serial', 'Go Command', u''],
             ['Simple Serial', 'Output Format', u''],
         ]
         for cmd in lstexample: self.api.setParameter(cmd)
-
         self.api.capture1()
         self.api.capture1()
         self.api.capture1()
 
+        print "Automatically Triggering Glitch - Enable the power analysis capture"
         lstexample = [
             ['OpenADC', 'Clock Setup', 'ADC Clock', 'Source', 'CLKGEN x4 via DCM'],
-            ['OpenADC', 'Clock Setup', 'ADC Clock', 'Reset ADC DCM', None],
+            ['OpenADC', 'Clock Setup', 'ADC Clock', 'Reset ADC DCM', None],  # Push the button
             ['OpenADC', 'Trigger Setup', 'Mode', 'rising edge'],
             ['OpenADC', 'Trigger Setup', 'Total Samples', 1000],
             ['OpenADC', 'Gain Setting', 'Setting', 40],
         ]
         for cmd in lstexample: self.api.setParameter(cmd)
+        self.api.capture1()
+        self.api.capture1()
+        self.api.capture1()
+        print self.api.getParameter(['CW Extra Settings', 'Trigger Pins', 'Target IO4 (Trigger Line)'])
 
-        self.api.capture1()
-        self.api.capture1()
-        self.api.capture1()
-
+        print "Automatically Triggering Glitch - Enable the trigger of the glitch to occur based on this external trigger pin"
         self.api.setParameter(['Glitch Module', 'Glitch Trigger', 'Ext Trigger:Single-Shot'])
-
         self.api.capture1()
         self.api.capture1()
         self.api.capture1()
 
+        print "Using the Glitch Explorer"
         self.api.setParameter(['Simple Serial', 'Output Format', u'$GLITCH$'])
-
         self.api.capture1()
         self.api.capture1()
         self.api.capture1()
-
         lstexample = [
             ['Glitch Explorer', 'Normal Response', u"s == '\\x00hello\\nA'"],
             ['Glitch Explorer', 'Successful Response', u"s.endswith('1234')"],
             ]
         for cmd in lstexample: self.api.setParameter(cmd)
-
         self.api.capture1()
         self.api.capture1()
         self.api.capture1()
 
+        print "Using the Glitch Explorer -  Tune the glitch offset to attempt to get a successful clock glitch"
         lstexample = [
             ['Glitch Explorer', 'Tuning Parameters', 1],
             ['Glitch Explorer', 'Tuning Parameter 0', 'Name', u'Offset'],
@@ -155,6 +159,34 @@ class UserScript(UserScriptBase):
         for cmd in lstexample: self.api.setParameter(cmd)
         self.api.captureM()
 
+        print "Using the Glitch Explorer - Tune the Glitch Width"
+        lstexample = [
+            ['Glitch Explorer', 'Tuning Parameters', 2],
+            ['Glitch Explorer', 'Tuning Parameter 1', 'Name', u'Width'],
+            ['Glitch Explorer', 'Tuning Parameter 1', 'Parameter Path', u"['Glitch Module', 'Glitch Width (as % of period)']"],
+            ['Glitch Explorer', 'Tuning Parameter 1', 'Data Format', 'Float'],
+            ['Glitch Explorer', 'Tuning Parameter 1', 'Range', (0, 9)],
+            ['Glitch Explorer', 'Tuning Parameter 1', 'Value', 0],
+            ['Glitch Explorer', 'Tuning Parameter 1', 'Step', 0.5],
+            ['Glitch Explorer', 'Tuning Parameter 1', 'Repeat', 1],
+            ['Glitch Explorer', 'Tuning Parameter 0', 'Range', (-7, 1)],
+            ['Glitch Explorer', 'Tuning Parameter 0', 'Value', -7],
+            ['Glitch Module', 'Repeat', 1],
+            ['Generic Settings', 'Acquisition Settings', 'Number of Traces', 323],
+        ]
+        for cmd in lstexample: self.api.setParameter(cmd)
+        self.api.captureM()
+
+        print "Using the Glitch Explorer - Fine-tune those values"
+        self.api.setParameter(['Glitch Module', 'Glitch Offset (as % of period)',-4])
+        self.api.setParameter(['Glitch Module', 'Glitch Width (as % of period)',8.5])
+        self.api.capture1()
+        self.api.capture1()
+        self.api.capture1()
+        self.api.setParameter(['Glitch Module', 'Glitch Offset (fine adjust)', 44])
+        self.api.capture1()
+        self.api.capture1()
+        self.api.capture1()
 
 if __name__ == '__main__':
     app = cwc.makeApplication()                     # Comment this line if you don't want to use the GUI
