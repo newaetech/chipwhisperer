d5aaa5e15575cbd2f0786f1870122a48728863de
Colin O'Flynn <coflynn@newae.com>
2016-06-29 07:52:36 -0300

    FW: Updates for target firmware: fix xmega build, fix ASM build

--- hardware/victims/firmware/hal/PLATFORM_INCLUDE.mk
+++ hardware/victims/firmware/hal/PLATFORM_INCLUDE.mk
@@ -6,8 +6,7 @@ ifeq ($(PLATFORM),CW301_AVR)
  HAL = avr
  PLTNAME = Multi-Target Board, AVR Target
 else ifeq ($(PLATFORM),CW301_XMEGA)
-#a4 not officially supported, but has same reg map
- MCU = atxmega128a3
+ MCU = atxmega16a4
  HAL = xmega
  PLTNAME = Multi-Target Board, XMEGA Target
 else ifeq ($(PLATFORM),CW303)
@@ -33,7 +32,7 @@ ifeq ($(HAL),avr)
  EXTRAINCDIRS += $(HALPATH)/avr
 else ifeq ($(HAL),xmega)
  VPATH=$(HALPATH)/xmega
- HALSRC = AES_driver.c uart.c usart_driver.c xmega_hal.c
+ HALSRC = XMEGA_AES_driver.c uart.c usart_driver.c xmega_hal.c
  EXTRAINCDIRS += $(HALPATH)/xmega
 else
  $(error: Unknown HAL: $(HAL))
