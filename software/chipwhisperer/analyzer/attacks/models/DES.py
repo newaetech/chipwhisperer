daea3e458f65cb1f2ab41326291e23faa01adfde
Adriel Ziesemer Jr <aziesemer@gmail.com>
2016-07-20 19:31:46 -0300

    Testing DES...

--- software/chipwhisperer/analyzer/attacks/models/DES.py
+++ software/chipwhisperer/analyzer/attacks/models/DES.py
@@ -28,21 +28,6 @@ from chipwhisperer.analyzer.models.aes.funcs import sbox, inv_sbox
 from chipwhisperer.analyzer.models.aes.key_schedule import keyScheduleRounds
 from chipwhisperer.common.utils import util
 
-LEAK_HW_SBOXOUT_FIRSTROUND = 1
-LEAK_HD_LASTROUND_STATE = 2
-LEAK_HD_SBOX_IN_OUT = 3
-LEAK_HD_SBOX_IN_SUCCESSIVE = 4
-LEAK_HD_SBOX_OUT_SUCCESSIVE = 5
-LEAK_HW_INVSBOXOUT_FIRSTROUND = 6
-
-leakagemodels = util.DictType()
-leakagemodels['HW: AES SBox Output, First Round (Enc)'] = 'LEAK_HW_SBOXOUT_FIRSTROUND'
-leakagemodels['HW: AES Inv SBox Output, First Round (Dec)'] = 'LEAK_HW_INVSBOXOUT_FIRSTROUND'
-leakagemodels['HD: AES Last-Round State'] = 'LEAK_HD_LASTROUND_STATE'
-leakagemodels['HD: AES SBox Input to Output'] = 'LEAK_HD_SBOX_IN_OUT'
-leakagemodels['HD: AES SBox Input i to i+1'] = 'LEAK_HD_SBOX_IN_SUCCESSIVE'
-leakagemodels['HD: AES SBox Output i to i+1'] = 'LEAK_HD_SBOX_OUT_SUCCESSIVE'
-
 numSubKeys = 16
 permPerSubkey = 256
 
@@ -53,18 +38,9 @@ for n in range(0, 256):
 
 
 def processKnownKey(setting, inpkey):
-
-    if setting == LEAK_HD_LASTROUND_STATE:
-        return keyScheduleRounds(inpkey, 0, 10)
-
-    return inpkey
+    return keyScheduleRounds(inpkey, 0, 10)
 
 
 def leakage(pt, ct, guess, bnum, setting, state):
-
-    if setting == LEAK_HW_SBOXOUT_FIRSTROUND:
-        # Classic HW of S-Box output
         return HW8Bit[sbox(pt[bnum] ^ guess)]
-    else:
-        raise ValueError("Invalid setting: %s" % str(setting))
 
