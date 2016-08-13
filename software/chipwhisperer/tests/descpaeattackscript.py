24117311201b3c88a81967928eb32af84d4e56de
Adriel Ziesemer Jr <aziesemer@gmail.com>
2016-08-13 20:23:28 -0300

    Creating DES scripts...

--- software/chipwhisperer/tests/descpaeattackscript.py
+++ software/chipwhisperer/tests/descpaeattackscript.py
@@ -20,8 +20,8 @@ class Capture(UserScriptBase):
 
     def run(self):
         # Deletes previous saved data
-        if os.path.isfile("projects/tut_cpa.cwp"): os.remove("projects/tut_cpa.cwp")
-        shutil.rmtree("projects/tut_cpa_data", ignore_errors=True)
+        if os.path.isfile("projects/tut_descpa.cwp"): os.remove("projects/tut_descpa.cwp")
+        shutil.rmtree("projects/tut_descpa_data", ignore_errors=True)
 
         # Setup the capture hardware
         self.api.setParameter(['Generic Settings', 'Scope Module', 'ChipWhisperer/OpenADC'])
@@ -39,69 +39,52 @@ class Capture(UserScriptBase):
         xmega._logging = None
         xmega.find()
         xmega.erase()
-        xmega.program(r"simpleserial-aes-xmega.hex", memtype="flash", verify=True)
+        xmega.program(r"simpleserial-des.hex", memtype="flash", verify=True)
         xmega.close()
 
         # Setup the capture parameters
-        lstexample = [['CW Extra Settings', 'Trigger Pins', 'Target IO4 (Trigger Line)', True],
-                      ['CW Extra Settings', 'Target IOn Pins', 'Target IO1', 'Serial RXD'],
-                      ['CW Extra Settings', 'Target IOn Pins', 'Target IO2', 'Serial TXD'],
-                      ['OpenADC', 'Clock Setup', 'CLKGEN Settings', 'Desired Frequency', 7370000.0],
-                      ['CW Extra Settings', 'Target HS IO-Out', 'CLKGEN'],
-                      ['OpenADC', 'Clock Setup', 'ADC Clock', 'Source', 'CLKGEN x4 via DCM'],
-                      ['OpenADC', 'Trigger Setup', 'Total Samples', 3000],
-                      ['OpenADC', 'Trigger Setup', 'Offset', 1250],
-                      ['OpenADC', 'Gain Setting', 'Setting', 45],
-                      ['OpenADC', 'Trigger Setup', 'Mode', 'rising edge'],
-                      #Final step: make DCMs relock in case they are lost
-                      ['OpenADC', 'Clock Setup', 'ADC Clock', 'Reset ADC DCM', None],
-                      ]
+        lstexample = [
+            ['CW Extra Settings', 'Trigger Pins', 'Target IO4 (Trigger Line)', True],
+            ['CW Extra Settings', 'Target IOn Pins', 'Target IO1', 'Serial RXD'],
+            ['CW Extra Settings', 'Target IOn Pins', 'Target IO2', 'Serial TXD'],
+            ['OpenADC', 'Clock Setup', 'CLKGEN Settings', 'Desired Frequency', 7370000.0],
+            ['CW Extra Settings', 'Target HS IO-Out', 'CLKGEN'],
+            ['OpenADC', 'Clock Setup', 'ADC Clock', 'Source', 'CLKGEN x4 via DCM'],
+            ['OpenADC', 'Trigger Setup', 'Total Samples', 3000],
+            ['OpenADC', 'Trigger Setup', 'Offset', 1250],
+            ['OpenADC', 'Gain Setting', 'Setting', 45],
+            ['OpenADC', 'Trigger Setup', 'Mode', 'rising edge'],
+            #Final step: make DCMs relock in case they are lost
+            ['OpenADC', 'Clock Setup', 'ADC Clock', 'Reset ADC DCM', None],
+        ]
         for cmd in lstexample: self.api.setParameter(cmd)
 
         # Capture a set of traces with fixed key and save the project
         self.api.newProject()
-        self.api.saveProject("projects/tut_cpa.cwp")
+        self.api.saveProject("projects/tut_descpa.cwp")
+        self.api.setParameter(['Simple Serial', 'Key Length (Bytes)', 8])
+        self.api.setParameter(['Simple Serial', 'Input Length (Bytes)', 8])
+        self.api.setParameter(['Simple Serial', 'Output Length (Bytes)', 8])
         self.api.setParameter(['Generic Settings', 'Basic', 'Key', 'Fixed'])
-        self.api.setParameter(['Generic Settings', 'Basic', 'Fixed Encryption Key', u'2B 7E 15 16 28 AE D2 A6 AB F7 15 88 09 CF 4F 3C'])
-        self.api.setParameter(['Generic Settings', 'Acquisition Settings', 'Number of Traces', 50])
+        self.api.setParameter(['Generic Settings', 'Basic', 'Fixed Encryption Key', u'2B 7E 15 16 28 AE D2 A6'])
+        self.api.setParameter(['Generic Settings', 'Acquisition Settings', 'Number of Traces', 100])
         time.sleep(1)
         self.api.captureM()
         self.api.saveProject()
 
 
 class Attack(UserScriptBase):
-    _name = "CPA Attack Analyzer Script"
+    _name = "DES-CPA Attack Analyzer Script"
 
     def initPreprocessing(self):
-        # Add amplitude noise and jitter to the original traces
-        ppMod0 = preprocessing.add_noise_jitter.AddNoiseJitter(self.api.project().traceManager())
-        ppMod0.setEnabled(True)
-        ppMod0.setMaxJitter(2)
-        ppMod0.init()
-        ppMod1 = preprocessing.add_noise_random.AddNoiseRandom(ppMod0)
-        ppMod1.setEnabled(False)
-        ppMod1.setMaxNoise(0.005000)
-        ppMod1.init()
-        self.traces = ppMod1
-
-    def initPreprocessing2(self):
-        # Resync using SAD (to fix the jitter) and applies decimation (because we want to test this feature :)
-        ppMod0 = preprocessing.resync_sad.ResyncSAD(self.api.project().traceManager())
-        ppMod0.setEnabled(True)
-        ppMod0.setReference(rtraceno=0, refpoints=(90,100), inputwindow=(70,120))
-        ppMod0.init()
-        ppMod1 = preprocessing.decimation_fixed.DecimationFixed(ppMod0)
-        ppMod1.setEnabled(True)
-        ppMod1.setDecimationFactor(2)
-        ppMod1.init()
-        self.traces = ppMod1
+        self.traces = self.api.project().traceManager()
 
     def initAnalysis(self):
         # Setup the CPA algorith
         self.attack = CPA()
-        self.attack.setAnalysisAlgorithm(CPAProgressive,chipwhisperer.analyzer.attacks.models.AES128_8bit,chipwhisperer.analyzer.attacks.models.AES128_8bit.LEAK_HW_SBOXOUT_FIRSTROUND)
+        self.attack.setAnalysisAlgorithm(chipwhisperer.analyzer.attacks.cpa_algorithms.progressive.CPAProgressive,chipwhisperer.analyzer.attacks.models.DES.DES,chipwhisperer.analyzer.attacks.models.DES.DES.LEAK_HW_SBOXOUT_FIRSTROUND)
         self.attack.setTraceStart(0)
-        self.attack.setTracesPerAttack(50)
+        self.attack.setTracesPerAttack(100)
         self.attack.setIterations(1)
         self.attack.setReportingInterval(10)
         self.attack.setTargetSubkeys([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
@@ -121,23 +104,8 @@ class Attack(UserScriptBase):
 
     def run(self):
         # This is what the API will execute
-        self.api.openProject("projects/tut_cpa.cwp")
+        self.api.openProject("projects/tut_descpa.cwp")
         self.initPreprocessing()
-
-        # In order to increase test coverage, we will save the trace with noise to the trace manager
-        self.api.getResults("Trace Recorder").setTraceSource(self.traces)
-        self.api.setParameter(['Results', 'Trace Recorder', 'Trace Format', 'ChipWhisperer/Native'])
-        self.api.setParameter(['Results', 'Trace Recorder', 'Trace Range', (0, 49)])
-        self.api.setParameter(['Results', 'Trace Recorder', 'Point Range', (0, 2999)])
-        self.api.setParameter(['Results', 'Trace Recorder', 'Save', None])
-
-        # Deselect the original traces and select this new one
-        self.api.project().traceManager().setTraceSegmentStatus(0, False)
-        self.api.project().traceManager().setTraceSegmentStatus(1, True)
-        self.api.saveProject()
-
-        # Fix the traces
-        self.initPreprocessing2()
 
         # Setup the analysis, widgets, and do the attack
         self.initAnalysis()
