84b4b73514855132e8cac2a24a1a16f04bc8ee20
Colin O'Flynn <coflynn@newae.com>
2017-04-11 00:50:33 -0300

    SW: Add V3.3.0 version numbers

--- software/chipwhisperer/common/api/CWCoreAPI.py
+++ software/chipwhisperer/common/api/CWCoreAPI.py
@@ -47,7 +47,7 @@ class CWCoreAPI(Parameterized):
 
     __name__ = "ChipWhisperer"
     __organization__ = "NewAE Technology Inc."
-    __version__ = "V3.2.0"
+    __version__ = "V3.3.0"
     _name = 'Generic Settings'
     instance = None
 
