f95bf306c647d1dd996911d82899c685f531070b
Colin O'Flynn <coflynn@newae.com>
2017-04-26 13:13:43 -0300

    SW: New ability to plot arbitrary traces, not just a range

--- software/chipwhisperer/common/results/waveform_widget.py
+++ software/chipwhisperer/common/results/waveform_widget.py
@@ -44,7 +44,8 @@ class WaveFormWidget(GraphWidget, ResultsBase, ActiveTraceObserver, Plugin):
 
         self.params.addChildren([
             {'name':'Redraw after Each', 'type':'bool', 'value':False},
-            {'name':'Trace Range', 'key':'tracerng', 'type':'range', 'limits':(0, 0), 'value':(0, 0)},
+            #{'name':'Trace Range', 'key':'tracerng', 'type':'range', 'limits':(0, 0), 'value':(0, 0)},
+            {'name':'Trace(s) to Plot', 'key':'tracecmd', 'type':'str', 'value':'0'},
             {'name':'Point Range', 'key':'pointrng', 'type':'rangegraph', 'limits':(0, 0), 'value':(0, 0), 'graphwidget':self},
             {'name':'Downsampling Mode', 'key':'dsmode', 'type':'list', 'values':{'None':None, 'Subsample':'subsample', 'Mean':'mean', 'Peak':'peak'},
                 'value':'peak', 'action':self.plotInputTrace},
@@ -77,14 +78,99 @@ class WaveFormWidget(GraphWidget, ResultsBase, ActiveTraceObserver, Plugin):
             lastTrace = -1
             lastPoint = -1
 
-        traceRange = self.findParam('tracerng').getValue()
-        self.findParam('tracerng').setLimits((0, lastTrace))
-        self.findParam('tracerng').setValue((max(0, traceRange[0]), min(lastTrace, traceRange[1] if traceRange[1] >= 0 else 7)))
+        #traceRange = self.findParam('tracerng').getValue()
+        #self.findParam('tracerng').setLimits((0, lastTrace))
+        #self.findParam('tracerng').setValue((max(0, traceRange[0]), min(lastTrace, traceRange[1] if traceRange[1] >= 0 else 7)))
+        self.findParam('tracecmd').setValue("0")
         self.findParam('pointrng').setLimits((0, lastPoint))
         self.findParam('pointrng').setValue((0, lastPoint))
 
+        mintrace = min(self.getPlotList())
+        maxtrace = max(self.getPlotList())
+
         self.findParam(['tstat', 'range']).setLimits((0, lastTrace))
-        self.findParam(['tstat', 'range']).setValue((max(0, traceRange[0]), min(lastTrace, traceRange[1] if traceRange[1] >= 0 else 7)))
+        self.findParam(['tstat', 'range']).setValue((max(0, mintrace), min(lastTrace, maxtrace) if maxtrace >= 0 else 7))
+
+    def top_level_split(self, s):
+        """
+        Split `s` by top-level commas only. Commas within parentheses are ignored.
+
+        Source: http://stackoverflow.com/questions/33527245/python-split-by-comma-skipping-the-content-inside-parentheses
+        """
+
+        # Parse the string tracking whether the current character is within
+        # parentheses.
+        balance = 0
+        parts = []
+        part = ''
+
+        for c in s:
+            part += c
+            if c == '(':
+                balance += 1
+            elif c == ')':
+                balance -= 1
+            elif c == ',' and balance == 0:
+                parts.append(part[:-1].strip())
+                part = ''
+
+        # Capture last part
+        if len(part):
+            parts.append(part.strip())
+
+        return parts
+
+    def getPlotList(self):
+        #Get range of traces used curently
+
+        tracecmd = self.findParam("tracecmd").getValue()
+        cmdparts = self.top_level_split(tracecmd)
+
+        #Example of valid plots:
+        #0
+        #2
+        #0-10
+        #0:10, 5, 9
+        #5('r', x=2), 9-10
+
+        plotlist = []
+        plotinfo = []
+        for cmd in cmdparts:
+
+            cmd_tracenum, sep, cmd_traceplt = cmd.partition("(")
+            cmd_traceplt = cmd_traceplt.rstrip(")")
+
+            #Parse range
+            cmd_tracenumint = cmd_tracenum.replace("-", " ")
+            cmd_tracenumint = cmd_tracenumint.replace(":", " ")
+            cmd_tracenumint = cmd_tracenumint.replace("to", " ")
+            cmd_tracenumint = cmd_tracenumint.split()
+
+            #Parse command
+            cmdtracedict = {'color':None,
+                            'xoffset':None}
+
+            if cmd_traceplt:
+                cmd_traceplotint = self.top_level_split(cmd_traceplt)
+                if cmd_traceplotint:
+                    cmdtracedict['color'] = cmd_traceplotint[0]
+
+            # Check if we have a range
+            if len(cmd_tracenumint) > 1:
+                if len(cmd_tracenumint) != 2:
+                    raise ValueError("Failed to parse: %s" % cmd_tracenum)
+                start = int(cmd_tracenumint[0])
+                end = int(cmd_tracenumint[1])
+                plotlist.extend(range(start, end))
+                plotinfo.extend([cmdtracedict] * (end-start))
+            else:
+                plotlist.append(int(cmd_tracenumint[0]))
+                plotinfo.append(cmdtracedict)
+
+
+                # TODO: Parse plot information
+
+        return (plotlist, plotinfo)
 
     def plotInputTrace(self, _=None):
         #print "Plotting %d-%d for points %d-%d"%(params[0].value(), params[1].value(), params[2].value(), params[3].value())
@@ -92,8 +178,7 @@ class WaveFormWidget(GraphWidget, ResultsBase, ActiveTraceObserver, Plugin):
         if not self.persistant:
             self.clearPushed()
 
-        tstart = self.findParam('tracerng').getValue()[0]
-        tend = self.findParam('tracerng').getValue()[1]
+        plotlist, plotinfo = self.getPlotList()
         pstart = self.findParam('pointrng').getValue()[0]
         pend = self.findParam('pointrng').getValue()[1]
         dsmode = self.findParam('dsmode').getValue()
@@ -104,11 +189,12 @@ class WaveFormWidget(GraphWidget, ResultsBase, ActiveTraceObserver, Plugin):
         ttstart = self.findParam(['tstat', 'range']).getValue()[0]
         ttend = self.findParam(['tstat', 'range']).getValue()[1]
 
-        if tend < tstart or pend < pstart:
+        #Sanity check
+        if pend < pstart or plotlist is None or len(plotlist) == 0:
             return
 
         try:
-            if tend - tstart + 1 > 1 or tstat_enabled:
+            if len(plotlist) > 1 or tstat_enabled:
                 self.setPersistance(True)
 
             yUnit = self.findParam(['Y Axis','Unity']).getValue()
@@ -124,13 +210,14 @@ class WaveFormWidget(GraphWidget, ResultsBase, ActiveTraceObserver, Plugin):
                                   'Change it to its actual value in the Trace Manager.' % self.getName())
                 xaxis = [v/tmp for v in xaxis]
 
-            for tnum in range(tstart, tend+1):
+            for i, tnum in enumerate(plotlist):
                 trace = self._traceSource.getTrace(tnum)
                 if trace is not None:
                     if yaxisScaleFactor != 1.0 or yaxisOffsetFactor != 0.0:
                         trace = [yaxisOffsetFactor + x * yaxisScaleFactor for x in trace]
                     #TODO - Not sure if should add _traceSource.offset() or not?
-                    self.passTrace(trace[pstart:pend+1], pstart + self._traceSource.offset(), idString = str(tnum), xaxis=xaxis, dsmode=dsmode)
+                    traceclr = plotinfo[i]['color']
+                    self.passTrace(trace[pstart:pend+1], pstart + self._traceSource.offset(), idString = str(tnum), xaxis=xaxis, dsmode=dsmode, color = traceclr)
 
                     if self.findParam('Redraw after Each').getValue():
                         util.updateUI()
