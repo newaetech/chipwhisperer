0f726cd317d55449007a1d08f0dc35202de71a56
Greg d'Eon <gdeon@newae.com>
2017-06-15 09:44:52 -0300

    SW: Add 87C51 setup script

---
+++ software/chipwhisperer/capture/scripts/87c51-setup.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2013-2017, NewAE Technology Inc
+# All rights reserved.
+#
+# Authors: Colin O'Flynn, Greg d'Eon
+#
+# Find this and more at newae.com - this file is part of the chipwhisperer
+# project, http://www.assembla.com/spaces/chipwhisperer
+#
+#    This file is part of chipwhisperer.
+#
+#    chipwhisperer is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    chipwhisperer is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with chipwhisperer.  If not, see <http://www.gnu.org/licenses/>.
+#=================================================
+
+from chipwhisperer.common.api.CWCoreAPI import CWCoreAPI  # Import the ChipWhisperer API
+from chipwhisperer.common.scripts.base import UserScriptBase
+
+
+class UserScript(UserScriptBase):
+    _name = "CW-Lite/Pro Setup for 87C51"
+    _description = "Hardware setup script for CW308T-87C51 target"
+
+    def __init__(self, api):
+        super(UserScript, self).__init__(api)
+
+    def run(self):
+        # User commands here
+        self.api.setParameter(['Generic Settings', 'Scope Module', 'ChipWhisperer/OpenADC'])
+        self.api.setParameter(['Generic Settings', 'Target Module', 'Simple Serial'])
+        self.api.setParameter(['Generic Settings', 'Trace Format', 'ChipWhisperer/Native'])
+        self.api.setParameter(['Simple Serial', 'Connection', 'NewAE USB (CWLite/CW1200)'])
+        self.api.setParameter(['ChipWhisperer/OpenADC', 'Connection', 'NewAE USB (CWLite/CW1200)'])
+                
+        self.api.connect()
+        
+        # Example of using a list to set parameters. Slightly easier to copy/paste in this format
+        lstexample = [['CW Extra Settings', 'Trigger Pins', 'Target IO4 (Trigger Line)', True],
+                      ['CW Extra Settings', 'Target IOn Pins', 'Target IO1', 'Serial RXD'],
+                      ['CW Extra Settings', 'Target IOn Pins', 'Target IO2', 'Serial TXD'],
+                      ['CW Extra Settings', 'Target IOn Pins', 'Target IO3', 'GPIO'],
+                      ['CW Extra Settings', 'Target IOn GPIO Mode', 'Target IO3: GPIO', 'High'],
+                      ['OpenADC', 'Clock Setup', 'CLKGEN Settings', 'Desired Frequency', 7370000.0],
+                      ['CW Extra Settings', 'Target HS IO-Out', 'CLKGEN'],
+                      ['OpenADC', 'Clock Setup', 'ADC Clock', 'Source', 'CLKGEN x4 via DCM'],
+                      ['Simple Serial', 'NewAE USB (CWLite/CW1200)', 'baud', 4800],
+                      ['Simple Serial', 'Protocol Version', 'Version', '1.1'],
+                      ['Simple Serial', 'Preset Mode', 'SimpleSerial Authentication'],
+                      ['OpenADC', 'Trigger Setup', 'Total Samples', 3000],
+                      ['OpenADC', 'Trigger Setup', 'Offset', 0],
+                      ['OpenADC', 'Gain Setting', 'Setting', 30],
+                      ['OpenADC', 'Trigger Setup', 'Mode', 'rising edge'],
+                      # Final step: make DCMs relock in case they are lost
+                      ['OpenADC', 'Clock Setup', 'ADC Clock', 'Reset ADC DCM', None],
+                      ]
+        
+        # Download all hardware setup parameters
+        for cmd in lstexample: self.api.setParameter(cmd)
+        
+        # Take device out of reset
+        self.api.setParameter(['CW Extra Settings', 'Target IOn GPIO Mode', 'Target IO3: GPIO', 'Low'])
+                      
+        # Throw away first few
+        self.api.capture1()
+        self.api.capture1()
+
+
+
+if __name__ == '__main__':
+    import chipwhisperer.capture.ui.CWCaptureGUI as cwc         # Import the ChipWhispererCapture GUI
+    from chipwhisperer.common.utils.parameter import Parameter  # Comment this line if you don't want to use the GUI
+    Parameter.usePyQtGraph = True                               # Comment this line if you don't want to use the GUI
+    api = CWCoreAPI()                                           # Instantiate the API
+    app = cwc.makeApplication("Capture")                        # Change the name if you want a different settings scope
+    gui = cwc.CWCaptureGUI(api)                                 # Comment this line if you don't want to use the GUI
+    api.runScriptClass(UserScript)                              # Run the User Script (executes "run()" by default)
+    app.exec_()                                                 # Comment this line if you don't want to use the GUI
