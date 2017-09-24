069f8bb600955fa26ac194f0943336f4acb41587
Colin O'Flynn <coflynn@newae.com>
2017-09-25 05:38:17 +0800

    CPA attack example: turn off added noise by default

--- software/chipwhisperer/analyzer/scripts/attack_cpa.py
+++ software/chipwhisperer/analyzer/scripts/attack_cpa.py
@@ -12,9 +12,10 @@ from chipwhisperer.analyzer.preprocessing.add_noise_random import AddNoiseRandom
 #self.project = cw.openProject("2017-mar23-xmega-aes.cwp")
 traces = self.project.traceManager()
 
+#Example: If you wanted to add noise, turn the .enabled to "True"
 self.ppmod[0] = AddNoiseRandom()
 self.ppmod[0].noise = 0.05
-self.ppmod[0].enabled = True
+self.ppmod[0].enabled = False
 
 attack = CPA()
 leak_model = AES128_8bit(SBox_output)
@@ -27,8 +28,6 @@ attack.setReportingInterval(10)
 attack.setTargetSubkeys([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
 attack.setTraceSource(self.ppmod[0])
 attack.setPointRange((0, 3000))
-
-
 
 self.results_table.setAnalysisSource(attack)
 self.correlation_plot.setAnalysisSource(attack)
