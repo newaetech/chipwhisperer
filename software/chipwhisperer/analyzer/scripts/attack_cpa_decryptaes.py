2ca7b297c39050d95b92b5fd53f737099f2dddf4
Colin O'Flynn <coflynn@newae.com>
2017-11-05 04:37:29 -0800

    SW: Analyzer Scripts: Add example aes decryption, update order of setting tracesource in DES

--- software/chipwhisperer/analyzer/scripts/attack_cpa_decryptaes.py
+++ software/chipwhisperer/analyzer/scripts/attack_cpa_decryptaes.py
@@ -1,26 +1,26 @@
 """CPA attack script.
 
-Assumes that a project with XMEGA software DES traces is already open.
+Assumes that a project with XMEGA software AES traces is already open.
 """
 
 import chipwhisperer as cw
 from chipwhisperer.analyzer.attacks.cpa import CPA
 from chipwhisperer.analyzer.attacks.cpa_algorithms.progressive import CPAProgressive
-from chipwhisperer.analyzer.attacks.models.DES import DES, SBox_output
+from chipwhisperer.analyzer.attacks.models.AES128_8bit import AES128_8bit, InvSBox_output
+from chipwhisperer.analyzer.preprocessing.add_noise_random import AddNoiseRandom
 
-#self.project = cw.openProject("2017-mar23-xmega-des.cwp")
+#self.project = cw.openProject("2017-mar23-xmega-aes.cwp")
 traces = self.project.traceManager()
 
 attack = CPA()
-leak_model = DES(SBox_output)
+leak_model = AES128_8bit(InvSBox_output)
 attack.setAnalysisAlgorithm(CPAProgressive, leak_model)
 attack.setTraceSource(traces)
-
 attack.setTraceStart(0)
 attack.setTracesPerAttack(-1)
 attack.setIterations(1)
 attack.setReportingInterval(10)
-attack.setTargetSubkeys([0, 1, 2, 3, 4, 5, 6, 7])
+attack.setTargetSubkeys([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
 attack.setPointRange((0, -1))
 
 self.results_table.setAnalysisSource(attack)
