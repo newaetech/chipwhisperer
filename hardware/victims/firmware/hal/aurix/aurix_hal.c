b79f9a0c29b06c826faa77f244b7d7b7a46d76ee
Alex Dewar <adewar@newae.com>
2019-05-27 14:07:12 -0300

    Fixed aurix hal to work @7.37MHz

--- hardware/victims/firmware/hal/aurix/aurix_hal.c
+++ hardware/victims/firmware/hal/aurix/aurix_hal.c
@@ -126,17 +126,20 @@ void init_uart(void)
 	//HighTec UART assumes 100MHz clock and uses prescale of 9+1, so we have prescale of 1+1
 	//to keep things the same
 	//prescale 9+1, oversample 16, sample position 7,8,9, 3 samples per bit
-	UART->BITCON.U = (9) | (15 << 16) | (9 << 24) | (1 << 31);
+	UART->BITCON.U = (1) | (15 << 16) | (9 << 24) | (1 << 31);
 
 	//8n1 UART
 	UART->FRAMECON.U = (1 << 9) | (0 << 16) | (0 << 30);
 
 	UART->DATCON.U = 7; //8bit data length
 
-#define BAUD_NUM (48 * 40)
-#define BAUD_DEN (3125)
+/* #define BAUD_NUM (48 * 40) */
+/* #define BAUD_DEN (3125) */
+#define BAUD_NUM (1001)
+#define BAUD_DEN (3002)
 /* #define BAUD_NUM (200) */
 /* #define BAUD_DEN (120) */
+  //NOTE: DEN>NUM
 
 	/*
 	 * fosc * num / ((prescale + 1) * den * (oversample + 1))
