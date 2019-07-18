0f263f1a8033677c4a047ee0ef88d55bc34c047d
Alex Dewar <adewar@newae.com>
2019-07-18 16:45:07 -0300

    Remove delay from serial write()

--- software/chipwhisperer/hardware/naeusb/serial.py
+++ software/chipwhisperer/hardware/naeusb/serial.py
@@ -119,8 +119,6 @@ class USART(object):
             self._usb.sendCtrl(self.CMD_USART0_DATA, 0, data[datasent:(datasent + datatosend)])
             datasent += datatosend
 
-        self.delay(0.0000001)
-
     def flush(self):
         """
         Flush all input buffers
