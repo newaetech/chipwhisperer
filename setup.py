775f898fff29af78f4214d86e078883091fbc60e
FranzHeubach <franz.heubach@gmail.com>
2019-07-25 11:21:47 -0300

    Moved setup.py to the root directory, updated setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='chipwhisperer',
+    version='5.1.0dev',
+    description="ChipWhisperer Side-Channel Analysis Tool",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Colin O'Flynn",
+    author_email='coflynn@newae.com',
+    license='GPLv3',
+    url='www.chipwhisperer.com',
+    packages=find_packages('software'),
+    package_dir={'': 'software'},
+    install_requires=[
+        'configobj',
+        'pyserial',
+        'docutils',  # makes help more readable.
+        'numpy',
+        'pyusb',
+    ],
+    project_urls={
+        'Documentation': 'https://chipwhisperer.readthedocs.io',
+        'Source': 'https://github.com/newaetech/chipwhisperer',
+        'Issue Tracker': 'https://github.com/newaetech/chipwhisperer/issues',
+    },
+    python_requires='~=3.4',
+)
