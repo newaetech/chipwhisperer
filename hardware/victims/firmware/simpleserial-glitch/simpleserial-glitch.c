82b8e08456c1b25df66e3731ffbad91b4f38bd85
Colin O'Flynn <coflynn@newae.com>
2020-05-02 22:00:11 -0300

    FW: Add new simpleserial-glitch application

---
+++ hardware/victims/firmware/simpleserial-glitch/simpleserial-glitch.c
@@ -0,0 +1,64 @@
+/*
+    This file is part of the ChipWhisperer Example Targets
+    Copyright (C) 2012-2017 NewAE Technology Inc.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "hal.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "simpleserial.h"
+
+uint8_t glitch_loop(uint8_t* in)
+{
+    volatile uint16_t i, j;
+    volatile uint32_t cnt;
+	cnt = 0;
+	trigger_high();
+	for(i=0; i<50; i++){
+		for(j=0; j<50; j++){
+			cnt++;
+		}
+	}
+	trigger_low();
+	simpleserial_put('r', 4, &cnt);
+	return 0;
+}
+
+
+int main(void)
+{
+    platform_init();
+	init_uart();	
+	trigger_setup();
+	
+ 	/* Device reset detected */
+	putch('r');
+	putch('R');
+	putch('E');
+	putch('S');
+	putch('E');
+	putch('T');
+	putch(' ');
+	putch(' ');
+    putch(' ');
+    putch('\n');
+    
+	simpleserial_init();
+	simpleserial_addcmd('g', 0, glitch_loop);
+	while(1)
+		simpleserial_get();
+}
