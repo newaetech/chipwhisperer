c33266e26a761ef9b7cb456f153c20a7a594926f
Colin O'Flynn <coflynn@newae.com>
2020-05-30 20:24:14 -0300

    FW: Add STM32L4 HAL support files

---
+++ hardware/victims/firmware/hal/stm32l4/stm32l4_hal.h
@@ -0,0 +1,17 @@
+#ifndef STM32L4_HAL_H
+#define STM32L4_HAL_H
+
+//You probably don't need this from rest of code
+//#include "stm32f4_hal_lowlevel.h"
+
+#include <stdint.h>
+
+void init_uart(void);
+void putch(char c);
+char getch(void);
+
+void trigger_setup(void);
+void trigger_low(void);
+void trigger_high(void);
+
+#endif // STM32L4_HAL_H
