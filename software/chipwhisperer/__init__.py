8fdd2e03d1b385fd85d06425cd17ad5b4d21d692
test <alexldewar@gmail.com>
2020-10-13 12:10:29 -0300

    Delay keyboard interrupt during cw.capture_trace()

--- software/chipwhisperer/__init__.py
+++ software/chipwhisperer/__init__.py
@@ -280,30 +280,50 @@ def capture_trace(scope, target, plaintext, key=None, ack=True):
     .. versionchanged:: 5.2
         Added ack parameter and use of target.output_len
     """
-    if key:
-        target.set_key(key, ack=ack)
 
-    scope.arm()
+    import signal, logging
 
-    if plaintext:
-        target.simpleserial_write('p', plaintext)
+    # useful to delay keyboard interrupt here,
+    # since could interrupt a USB operation
+    # and kill CW until unplugged+replugged
+    class DelayedKeyboardInterrupt:
+        def __enter__(self):
+            self.signal_received = False
+            self.old_handler = signal.signal(signal.SIGINT, self.handler)
 
-    ret = scope.capture()
+        def handler(self, sig, frame):
+            self.signal_received = (sig, frame)
+            logging.debug('SIGINT received. Delaying KeyboardInterrupt.')
 
-    i = 0
-    while not target.is_done():
-        i += 1
-        time.sleep(0.05)
-        if i > 100:
-            warnings.warn("Target did not finish operation")
+        def __exit__(self, type, value, traceback):
+            signal.signal(signal.SIGINT, self.old_handler)
+            if self.signal_received:
+                self.old_handler(*self.signal_received)
+    with DelayedKeyboardInterrupt():
+        if key:
+            target.set_key(key, ack=ack)
+
+        scope.arm()
+
+        if plaintext:
+            target.simpleserial_write('p', plaintext)
+
+        ret = scope.capture()
+
+        i = 0
+        while not target.is_done():
+            i += 1
+            time.sleep(0.05)
+            if i > 100:
+                warnings.warn("Target did not finish operation")
+                return None
+
+        if ret:
+            warnings.warn("Timeout happened during capture")
             return None
 
-    if ret:
-        warnings.warn("Timeout happened during capture")
-        return None
-
-    response = target.simpleserial_read('r', target.output_len, ack=ack)
-    wave = scope.get_last_trace()
+        response = target.simpleserial_read('r', target.output_len, ack=ack)
+        wave = scope.get_last_trace()
 
     if len(wave) >= 1:
         return Trace(wave, plaintext, response, key)
