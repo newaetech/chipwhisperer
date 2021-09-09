cbd21c9e0336f928012b27aa0ea25fd5cdb62bf0
Alex Dewar <alexldewar@gmail.com>
2021-09-09 20:31:42 -0300

    Fix baud for HSE STM32F4

--- hardware/victims/firmware/hal/stm32f4/stm32f4_hal.c
+++ hardware/victims/firmware/hal/stm32f4/stm32f4_hal.c
@@ -86,8 +86,8 @@ void platform_init(void)
 	RCC_ClkInitStruct.ClockType      = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
 	RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_HSE;
 	RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_ACR_LATENCY_5WS);
 #endif
 
