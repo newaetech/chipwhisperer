3420466765283daab41f330b4f1965f397fbb9a9
Alex Dewar <alexldewar@gmail.com>
2023-02-21 20:23:45 -0400

    change from caddr_t to char *

--- hardware/victims/firmware/hal/stm32f3/stm32f3_sysmem.c
+++ hardware/victims/firmware/hal/stm32f3/stm32f3_sysmem.c
@@ -13,7 +13,7 @@
 **
 **  Environment : System Workbench for MCU
 **
-**  Distribution: The file is distributed “as is,” without any warranty
+**  Distribution: The file is distributed ï¿½as is,ï¿½ without any warranty
 **                of any kind.
 **
 *****************************************************************************
@@ -60,7 +60,7 @@ register char * stack_ptr asm("sp");
  _sbrk
  Increase program data space. Malloc and related functions depend on this
 **/
-caddr_t _sbrk(int incr)
+char *_sbrk(int incr)
 {
 	extern char end asm("end");
 	static char *heap_end;
@@ -73,11 +73,11 @@ caddr_t _sbrk(int incr)
 	if (heap_end + incr > stack_ptr)
 	{
 		errno = ENOMEM;
-		return (caddr_t) -1;
+		return (char *) -1;
 	}
 
 	heap_end += incr;
 
-	return (caddr_t) prev_heap_end;
+	return (char *) prev_heap_end;
 }
 
