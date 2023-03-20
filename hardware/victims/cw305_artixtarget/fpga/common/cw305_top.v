823a76c9b594590215c0bdc6c7746e64077e65ed
jpcrypt <jpthibault@newae.com>
2023-03-20 12:04:26 -0400

    Fix https://github.com/newaetech/chipwhisperer/issues/435
    (inadvertently introduced by 5bf482da14786816d2f92ecf869223871a6387b3)

--- hardware/victims/cw305_artixtarget/fpga/common/cw305_top.v
+++ hardware/victims/cw305_artixtarget/fpga/common/cw305_top.v
@@ -38,7 +38,9 @@ module cw305_top #(
 )(
     // USB Interface
     input wire                          usb_clk,        // Clock
+`ifdef SS2_WRAPPER
     output wire                         usb_clk_buf,    // if needed by parent module
+`endif
     inout wire [7:0]                    usb_data,       // Data for write/read
     input wire [pADDR_WIDTH-1:0]        usb_addr,       // Address
     input wire                          usb_rdn,        // !RD, low when addr valid for read
@@ -80,6 +82,9 @@ module cw305_top #(
 `endif
     );
 
+`ifndef SS2_WRAPPER
+    wire usb_clk_buf;
+`endif
 
     wire [pKEY_WIDTH-1:0] crypt_key;
     wire [pPT_WIDTH-1:0] crypt_textout;
