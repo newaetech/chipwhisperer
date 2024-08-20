5af72eb14492b0690297a03942dbedfefe6ae674
jpcrypt <jpthibault@newae.com>
2024-08-20 14:33:20 -0400

    SAD explorer: various improvements.

--- software/chipwhisperer/common/utils/sad_explorer.py
+++ software/chipwhisperer/common/utils/sad_explorer.py
@@ -53,7 +53,7 @@ class SADExplorer(util.DisableNewAttr):
 
     _name = 'Husky SAD Explorer Module'
 
-    def __init__(self, scope, target, reftrace, refstart, max_segments=1, plot_width=2000, plot_height=600, plot_tools='pan, box_zoom, hover, reset, save'):
+    def __init__(self, scope, target, reftrace, refstart, max_segments=1, plot_width=2000, plot_height=600, plot_tools='pan, box_zoom, hover, reset, save', capture_function=None):
         super().__init__()
         self.scope = scope
         self.target = target
@@ -61,6 +61,8 @@ class SADExplorer(util.DisableNewAttr):
         self.reftrace = reftrace
         self.refstart = refstart
         self.max_segments = max_segments
+        self._max_legend_segments = 10
+        self.capture_function = capture_function
         self.p = figure(plot_width=plot_width, plot_height=plot_height, tools=plot_tools)
 
         self.samples = 100 # initial value really doesn't matter
@@ -96,20 +98,26 @@ class SADExplorer(util.DisableNewAttr):
 
         my_interact_manual = interact_manual.options(manual_name="run SAD capture")
         my_interact_manual(self.update_plot, 
-                           refstart =           widgets.Text(value='14570', description='reference start sample', style=style, layout=layout),
+                           refstart =           widgets.Text(value=str(refstart), description='reference start sample', style=style, layout=layout),
                            samples =            widgets.Text(value=str(scope.SAD.sad_reference_length), description='scope.adc.samples', style=style, layout=layout),
-                           segments =           widgets.Text(value='8', description='scope.adc.segments', style=style, layout=layout),
-                           threshold =          widgets.Text(value='20', description='scope.SAD.threshold', style=style, layout=layout),
-                           interval_threshold = widgets.Text(value='16', description='scope.SAD.interval_threshold', style=style, layout=layout),
+                           segments =           widgets.Text(value=str(max_segments), description='scope.adc.segments', style=style, layout=layout),
+                           threshold =          widgets.Text(value=str(scope.SAD.threshold), description='scope.SAD.threshold', style=style, layout=layout),
+                           interval_threshold = widgets.Text(value=str(scope.SAD.interval_threshold), description='scope.SAD.interval_threshold', style=style, layout=layout),
                            exclude =            widgets.Text(value='', description='excluded samples', style=style, layout=layout),
-                           emode =              widgets.Checkbox(value=False, description='scope.SAD.emode', style=style, layout=layout), 
+                           emode =              widgets.Checkbox(value=scope.SAD.emode, description='scope.SAD.emode', style=style, layout=layout), 
+                           always_armed =       widgets.Checkbox(value=scope.SAD.always_armed, description='scope.SAD.always_armed', style=style, layout=layout), 
                            show_diffs =         widgets.Checkbox(value=False, description='show diff', style=style, layout=layout),
-                           show_text_legend =   widgets.Checkbox(value=False, description='show text legend (slower)', style=style, layout=layout),
-                           show_plot_legend =   widgets.Checkbox(value=False, description='show plot legend (slowest)', style=style, layout=layout))
+                           show_text_legend =   widgets.Checkbox(value=False, description='show text legend (slow)', style=style, layout=layout),
+                           show_plot_legend =   widgets.Checkbox(value=False, description='show plot legend (slower)', style=style, layout=layout),
+                           legend_sad_stats =   widgets.Checkbox(value=False, description='include SAD stats in legend (slowest)', style=style, layout=layout))
 
         display(self.textout)
         display(self.captureout)
         self.disable_newattr()
+
+    @property
+    def legend_segments(self):
+        return min(self._max_legend_segments, self.max_segments)
 
 
     def calc_sad(self, wave):
@@ -126,40 +134,57 @@ class SADExplorer(util.DisableNewAttr):
         return sad, max(exceeds)
 
 
-    def get_legend_items(self, segments):
+    def get_legend_items(self, segments, sad_stats):
         items = []
         ttimes = self.scope.trigger.get_trigger_times()
-        #ttimes = [0]*(self.max_segments-1)
-        #print('got ttimes: %s' % ttimes)
         sads = []
-        for i in range(len(segments)):
-            sad, max_exceeds = self.calc_sad(segments[i])
-            sads.append(sad)
+        for i in range(min(len(segments), self.legend_segments)):
+        #for i in range(self.legend_segments):
+            if sad_stats:
+                sad, max_exceeds = self.calc_sad(segments[i])
+                sads.append(sad)
             if i == 0:
                 delta = 0
             elif ttimes:
                 delta = ttimes[i-1]
             else:
                 delta = 0
-            items.append('segment %d: SAD=%d, max over ref=%d, delta=%d' % (i, sad, max_exceeds, delta))
-        if sads:
+            if sad_stats:
+                items.append('segment %d: SAD=%d, max over ref=%d, delta=%d' % (i, sad, max_exceeds, delta))
+            else:
+                items.append('segment %d: delta=%d' % (i, delta))
+        if sad_stats:
             items.append('SADs: %d / %d / %d' % (min(sads), max(sads), int(np.average(sads))))
         else:
             items.append('SADs: N/A')
+
         if ttimes:
-            items.append('ttimes: %d / %d / %d' % (min(ttimes), max(ttimes), int(np.average(ttimes))))
+            items.append('trigger times: %d / %d / %d' % (min(ttimes), max(ttimes), int(np.average(ttimes))))
         else:
-            items.append('ttimes: N/A')
+            items.append('trigger times: N/A')
+        items.append('scope.SAD.num_triggers_seen: %d' % self.scope.SAD.num_triggers_seen)
         items.append('scope.adc.errors: %s' % self.scope.adc.errors)
         return items
                          
 
     def create_legend(self):
-        legend_items = [ LegendItem(label='', renderers=[]) ]*(self.max_segments + 3)
+        legend_items = [ LegendItem(label='', renderers=[]) ]*(self.legend_segments + 4)
         return Legend(items=legend_items)
 
 
-    def update_plot(self, refstart='', samples='', segments='', threshold='', interval_threshold='', exclude='', emode=False, show_diffs=False, show_text_legend=False, show_plot_legend=False):
+    def update_plot(self, 
+                    refstart='', 
+                    samples='', 
+                    segments='', 
+                    threshold='', 
+                    interval_threshold='', 
+                    exclude='', 
+                    emode=False, 
+                    always_armed=False, 
+                    show_diffs=False, 
+                    show_text_legend=False, 
+                    show_plot_legend=False,
+                    legend_sad_stats=False):
         segments = int(segments)
         threshold = int(threshold)
         interval_threshold = int(interval_threshold)
@@ -167,6 +192,7 @@ class SADExplorer(util.DisableNewAttr):
         self.SAD.threshold = threshold
         self.SAD.interval_threshold = interval_threshold
         self.SAD.emode = emode
+        self.SAD.always_armed = always_armed
 
         # visually indicate that a new capture has started::
         self.p.background_fill_color = 'yellow'
@@ -178,7 +204,8 @@ class SADExplorer(util.DisableNewAttr):
         refstart = int(refstart)
         if self.refstart != refstart: # no need to update if it hasn't changed
             self.refstart = refstart
-            self.SAD.reference = self.reftrace[refstart:]
+            with self.captureout:
+                self.SAD.reference = self.reftrace[refstart:]
 
         presamples = self.SAD.sad_reference_length + self.SAD.latency
         samples = max(samples, presamples+2)
@@ -189,8 +216,14 @@ class SADExplorer(util.DisableNewAttr):
         self.scope.adc.presamples = presamples
 
         self.captureout.clear_output()
+        trace = None
         with self.captureout:
-            trace = cw.capture_trace(self.scope, self.target, bytearray(16), bytearray(16), as_int=True)
+            if self.capture_function is None:
+                trace = cw.capture_trace(self.scope, self.target, bytearray(16), bytearray(16), as_int=True)
+            else:
+                trace = self.capture_function()
+            if self.scope.adc.errors:
+                print('scope.adc.errors: %s' % self.scope.adc.errors)
 
         if show_diffs:
             self.Rp.data_source.data = {'y': [interval_threshold]*samples,
@@ -202,12 +235,19 @@ class SADExplorer(util.DisableNewAttr):
                                         'x': range(samples)}
 
         else:
-            self.Rp.data_source.data = {'y': self.reftrace[refstart:refstart+samples] + interval_threshold,
+            # prevent over/underflow!
+            top = self.reftrace[refstart:refstart+samples] + interval_threshold
+            bottom = self.reftrace[refstart:refstart+samples] - interval_threshold
+
+            top[top < self.reftrace[refstart:refstart+samples]] = 255
+            bottom[bottom > self.reftrace[refstart:refstart+samples]] = 0
+
+            self.Rp.data_source.data = {'y': top,
                                         'x': range(samples)}
-            self.Rm.data_source.data = {'y': self.reftrace[refstart:refstart+samples] - interval_threshold,
+            self.Rm.data_source.data = {'y': bottom,
                                         'x': range(samples)}
-            self.Rf.data_source.data = {'y1': self.reftrace[refstart:refstart+samples] - interval_threshold,
-                                        'y2': self.reftrace[refstart:refstart+samples] + interval_threshold,
+            self.Rf.data_source.data = {'y1': bottom,
+                                        'y2': top,
                                         'x': range(samples)}
 
         if trace is None:
@@ -242,7 +282,7 @@ class SADExplorer(util.DisableNewAttr):
 
             self.textout.clear_output()
             if show_text_legend or show_plot_legend:
-                items = self.get_legend_items(segments)
+                items = self.get_legend_items(segments, legend_sad_stats)
 
             if show_text_legend:
                 with self.textout:
@@ -251,12 +291,13 @@ class SADExplorer(util.DisableNewAttr):
 
             if show_plot_legend:
                 self.p.legend.visible = True
-                for i in range(self.max_segments):
+                for i in range(self.legend_segments):
                     if i < self.scope.adc.segments:
                         self.p.legend.items[i] = LegendItem(label=items[i], renderers=[self.S[i]])
                     else:
                         self.p.legend.items[i] = (LegendItem(label='', renderers=[]))
                 # update the legend min/max lines:
+                self.p.legend.items[-4] = LegendItem(label=items[-4], renderers=[])
                 self.p.legend.items[-3] = LegendItem(label=items[-3], renderers=[])
                 self.p.legend.items[-2] = LegendItem(label=items[-2], renderers=[])
                 self.p.legend.items[-1] = LegendItem(label=items[-1], renderers=[])
