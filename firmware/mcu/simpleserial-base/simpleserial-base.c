78e4f83ce3d9e85115b20834292101b509a8e5c5
Alex Dewar <alexldewar@gmail.com>
2025-05-07 18:32:24 -0300

    Simplify base firmware

--- firmware/mcu/simpleserial-base/simpleserial-base.c
+++ firmware/mcu/simpleserial-base/simpleserial-base.c
@@ -53,45 +53,11 @@ uint8_t reset(uint8_t* x, uint8_t len)
 	return 0x00;
 }
 
-#if SS_VER == SS_VER_2_1
-uint8_t aes(uint8_t cmd, uint8_t scmd, uint8_t len, uint8_t *buf)
-{
-    uint8_t req_len = 0;
-    uint8_t err = 0;
-
-    if (scmd & 0x02) {
-        req_len += 16;
-        if (req_len > len) {
-            return SS_ERR_LEN;
-        }
-        err = get_key(buf + req_len - 16, 16);
-        if (err)
-            return err;
-    }
-    if (scmd & 0x01) {
-        req_len += 16;
-        if (req_len > len) {
-            return SS_ERR_LEN;
-        }
-        err = get_pt(buf + req_len - 16, 16);
-        if (err)
-            return err;
-    }
-
-    if (len != req_len) {
-        return SS_ERR_LEN;
-    }
-
-    return 0x00;
-
-}
-#endif
-
 int main(void)
 {
-    platform_init();
-	init_uart();
-	trigger_setup();
+    platform_init(); // setup clocks, IO pins
+	init_uart(); // setup UART
+	trigger_setup(); // setup trigger pin, allows trigger_high() and trigger_low() to work
 
  	/* Uncomment this to get a HELLO message for debug */
 	/*
@@ -104,14 +70,10 @@ int main(void)
 	*/
 
 	simpleserial_init();
-#if SS_VER != SS_VER_2_1
 	simpleserial_addcmd('p', 16, get_pt);
 	simpleserial_addcmd('k', 16, get_key);
 	simpleserial_addcmd('x', 0, reset);
-#else
-    simpleserial_addcmd(0x01, 16, aes);
 
-#endif
 	while(1)
 		simpleserial_get();
 }
